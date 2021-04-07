c2758daa6e69704de51f9d230fbd129b3d285968
서현 김 <atlk93@knou.ac.kr>
2021-04-07 15:36:27 +0900

    .

--- ComClient/FormClient.cs
+++ ComClient/FormClient.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,11 @@ namespace ComClient
 {
     public partial class FormClient : Form
     {
+        [DllImport("kernel32")]
+        static extern int GetPrivateProfileString(string sec, string key, string defStr, StringBuilder sb, int nsb, string path);
+        [DllImport("kernel32")]
+        static extern int WritePrivateProfileString(string sec, string key, string str, string path);
+
         public FormClient()
         {
             InitializeComponent();
